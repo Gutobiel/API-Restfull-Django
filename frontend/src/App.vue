e4d2c8c905ad09ba7a5ad65d9b10e86fb047d98a
Augusto Gabriel Rodrigues dos Santos <augusto.santos@dnit.gov.br>
2025-08-20 13:34:57 -0300

    Adicionado footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,10 +3,12 @@
   <div id="app">
     <Header v-if="!$route.meta.hideNavbar" />
     <router-view />
+    <Footer/>
   </div>
 </template>
 
 <script setup>
+import Footer from './components/Footer.vue';
 import Header from './components/Header.vue'
 </script>
 
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer class="footer d-flex align-items-center">
+        <p><strong>Copyright © 2025 Dnit - Departamento Nacional de Infraestrutura de Transportes - 2025. </strong> All rights reserved.</p>
+            <div class=" d-sm-inline">
+            <b>Crud version</b> 1.0.0
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+.footer {
+    justify-content: space-between;
+    align-items: center;
+    background-color: #f5f5f5;
+    padding: 10px;
+    font-size: 14px;
+    border-top: 1px solid #e0e0e0;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    z-index: 100;
+}
+</style>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
